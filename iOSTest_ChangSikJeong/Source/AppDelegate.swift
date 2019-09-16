2e1b6c01500adb3822cd53b3d3e17f0c7e8fca36
changSic <a920506@gmail.com>
2019-09-16 15:51:06 +0900

    doc: Initialize repository

---
+++ iOSTest_ChangSikJeong/Source/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  iOSTest_ChangSikJeong
+//
+//  Created by Sicc on 16/09/2019.
+//  Copyright © 2019 chang sic jung. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
+    window?.backgroundColor = .white
+    
+    
+    return true
+  }
+
+
+}
+
---
+++ iOSTest_ChangSikJeong/Source/ContentsListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContentsListViewController.swift
+//  iOSTest_ChangSikJeong
+//
+//  Created by Sicc on 16/09/2019.
+//  Copyright © 2019 chang sic jung. All rights reserved.
+//
+
+import UIKit
+
+class ContentsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
